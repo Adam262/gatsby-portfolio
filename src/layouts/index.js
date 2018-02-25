27d5121607aafc1b53a093602357f9e3f8faa47f
Adam Barcan <abarcan@gmail.com>
2018-02-25 13:45:28 -0500

    First commit
    * Add Gatsby boilerplate + placeholder pages
    * Ad fonts
    * Install styled-components

---
+++ src/layouts/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+
+import Header from '../components/Header'
+
+const TemplateWrapper = ({ children }) => (
+  <div>
+    <Helmet
+      title="Adam Barcan"
+    />
+    <Header />
+    <div>
+      {children()}
+    </div>
+  </div>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
