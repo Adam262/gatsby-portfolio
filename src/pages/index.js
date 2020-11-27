cb5e6740d2c9615e859e1735cab9e85bd6f9be9f
Adam Barcan <adam@greenhouse.io>
2020-11-27 18:38:12 -0500

    Upgrade skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ const IndexPage = () => (
   <div>
     <Card title="Fullstack developer in NYC">
       <p> 
-        I'm a full-stack software engineer who mainly works in Ruby on Rails, Postgres and React. 
+        I&#39;m a full-stack + DevOps software engineer with deep experience in Ruby on Rails, React, Kubernetes and Terraform. 
 
         With a strong background in automated testing, I value writing code that is well-tested, coherent, and maintainable. 
 
-        Outside of coding, I'm a lifelong competitive runner, and a photographer of sports and music events. 
+        Outside of coding, I&#39;m a lifelong competitive runner, and a photographer of sports and music events. 
        </p>
     </Card>
     <Scene/>
